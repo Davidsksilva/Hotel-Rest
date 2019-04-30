5f6ac0b6d8779f2346333f5612038e5eb2199e98
David <davidsimonkastle@gmail.com>
2019-04-30 16:25:28 -0300

    Database Dummies Loader

--- src/main/java/com/project/hotelrest/LoadDatabase.java
+++ src/main/java/com/project/hotelrest/LoadDatabase.java
@@ -9,14 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 @Configuration
 @Slf4j
 public class LoadDatabase {
 
     private static int getRandomIntegerBetweenRange(int min, int max){
-        int x = (int)(Math.random()*((max-min)+1))+min;
-        return x;
+        return (int)(Math.random()*((max-min)+1))+min;
+    }
+
+    private boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
     }
 
     // Populate Database
@@ -29,17 +34,55 @@ public class LoadDatabase {
                     "DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RR","RO",
                     "RJ","RN","RS","SC","SP","SE","TO");
 
+            List<String> sex_list = Arrays.asList("Male", "Female", "Other");
 
+            Faker faker = new Faker(new Locale("pt"));
 
-            Faker faker = new Faker(new Locale("pt-BR"));
-
+            // Hotel Loop
             for(int i=0; i<30; i++){
 
+                // Random UF index
                 int uf_index = getRandomIntegerBetweenRange(0,25);
+
+                // Generating Random entry values for Hotel entity
                 int stars = getRandomIntegerBetweenRange(1,5);
                 String uf = uf_list.get(uf_index);
+                String name = faker.company().name();
 
-                log.info("Preloading " + h_repository.save(new Hotel ("Hotel Cabo Branco", stars, uf)));
+                // Create Hotel entity
+                Hotel hotel = new Hotel (name, stars, uf);
+                log.info("Preloading " + h_repository.save(hotel));
+
+                //Generating Random entry values for Bedroom entities
+                int num_bedrooms = getRandomIntegerBetweenRange(1,20);
+
+                // Bedroom Loop
+                for(int j = 1; j <= num_bedrooms; j++){
+                    boolean occupied = getRandomBoolean();
+                    int num_beds = getRandomIntegerBetweenRange(1,4);
+                    int price = getRandomIntegerBetweenRange(10,50) * 10;
+
+                    if(occupied){
+
+                        // Generating Random entry values for Guest entity
+
+                        String guest_name = faker.name().fullName();
+                        int age = getRandomIntegerBetweenRange(20,70);
+                        int sex_index = getRandomIntegerBetweenRange(0,2);
+                        String sex = sex_list.get(sex_index);
+                        Guest guest = new Guest (guest_name, age, sex);
+
+                        log.info("Preloading " + g_repository.save(guest));
+
+                        log.info("Preloading " + br_repository.save(new Bedroom(j,num_beds, price,true,hotel,guest)));
+
+                    }
+                    else{
+                        log.info("Preloading " + br_repository.save(new Bedroom(j,num_beds, price,false,hotel,null)));
+                    }
+
+
+                }
             }
 
 
@@ -47,7 +90,7 @@ public class LoadDatabase {
 
 
 
-            log.info("Preloading " + h_repository.save(new Hotel ("Hotel Cabo Branco", 5, "PB")));
+            /*log.info("Preloading " + h_repository.save(new Hotel ("Hotel Cabo Branco", 5, "PB")));
             Guest guest = new Guest (faker.name().fullName(), 21, "Male");
             log.info("Preloading " + g_repository.save(guest));
             List<Hotel> hotels = h_repository.findAll();
@@ -55,8 +98,7 @@ public class LoadDatabase {
                 log.info("Preloading " + br_repository.save(new Bedroom(1,1, 100,true,hotel,guest)));
                 log.info("Preloading " + br_repository.save(new Bedroom(2,2, 200,true,hotel,guest)));
 
-            }
-            //log.info("Preloading " + repository.save(new Hotel ("Hotel Cabo Branco", 5, "PB")));
+            }*/
         };
     }
 }
