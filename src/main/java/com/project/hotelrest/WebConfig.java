46db7be09f82fb0c038cfc1f85bcb685c33917ba
David <davidsimonkastle@gmail.com>
2019-05-29 13:13:16 -0300

    CORS and webSecurity comments

--- src/main/java/com/project/hotelrest/WebConfig.java
+++ src/main/java/com/project/hotelrest/WebConfig.java
@@ -2,12 +2,10 @@ package com.project.hotelrest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-
+// Class to enable CORS
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/project/hotelrest/WebSecurityConfiguration.java
+++ src/main/java/com/project/hotelrest/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+// Class to enable requests on all Endpoints
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
